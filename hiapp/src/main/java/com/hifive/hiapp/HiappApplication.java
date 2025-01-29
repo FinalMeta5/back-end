f956cc5ad1e2ad21c9f52c45aea18f090d7716c3
gusals <gusals6586@gmail.com>
2025-01-29 14:17:49 +0900

    Fix: 애플리케이션 두 번 기동되는 문제 해결 시도

--- hiapp/src/main/java/com/hifive/hiapp/HiappApplication.java
+++ hiapp/src/main/java/com/hifive/hiapp/HiappApplication.java
@@ -1,8 +1,5 @@
 package com.hifive.hiapp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -14,18 +11,4 @@ public class HiappApplication extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(HiappApplication.class);
 	}
-
-	// 로깅 파일 저장 확인용
-    private static final Logger logger = LoggerFactory.getLogger(HiappApplication.class);
-
-	public static void main(String[] args) {
-		SpringApplication.run(HiappApplication.class, args);
-		/*//로깅 파일 저장 확인용*/
-		 logger.trace("TRACE 로그 - 가장 상세한 디버깅 정보");
-		 logger.debug("DEBUG 로그 - 디버깅에 유용한 정보");
-		 logger.info("INFO 로그 - 애플리케이션 상태 정보"); logger.warn("WARN 로그 - 잠재적인 문제 경고");
-		 logger.error("ERROR 로그 - 심각한 문제 발생");
-		 
-	}
-
 }
