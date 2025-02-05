3f05e36821a8f8d5f348d5f7bd5dcc135d9490e4
gusals <gusals6586@gmail.com>
2025-02-05 15:40:36 +0900

    Remove: 파일 삭제

--- hiapp/src/main/java/com/hifive/bururung/domain/member/entity/MemberState.java
+++
@@ -1,6 +0,0 @@
-package com.hifive.bururung.domain.member.entity;
-
-public enum MemberState {
-	ACTIVE,
-	INACTIVE
-}
--- hiapp/src/main/java/com/hifive/bururung/domain/member/service/IMemberService.java
+++
@@ -1,9 +0,0 @@
-package com.hifive.bururung.domain.member.service;
-
-import com.hifive.bururung.domain.member.dto.SignupRequest;
-import com.hifive.bururung.domain.member.dto.TokenDTO;
-
-public interface IMemberService {
-	TokenDTO login(String email, String password);
-	Long signup(SignupRequest signupRequest);
-}
