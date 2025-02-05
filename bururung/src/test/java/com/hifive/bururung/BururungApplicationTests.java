9e9ccf9a2ba88847e02af4a8664c71cfa4bab78c
gusals <gusals6586@gmail.com>
2025-02-05 21:22:16 +0900

    Rename: 패키지명 변경

--- bururung/src/test/java/com/hifive/bururung/BururungApplicationTests.java
+++ bururung/src/test/java/com/hifive/bururung/BururungApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hifive.hiapp;
+package com.hifive.bururung;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HiappApplicationTests {
+class BururungApplicationTests {
 
 	@Test
 	void contextLoads() {
