52e950decfab173c2afccb3fcf475fabad6a858e
nayeon990315 <99skdus@naver.com>
2025-02-07 22:17:55 +0900

    Resolve merge conflict

--- bururung/src/main/java/com/hifive/bururung/domain/carshare/participant/service/IServiceRegistrationService.java
+++ bururung/src/main/java/com/hifive/bururung/domain/carshare/participant/service/IServiceRegistrationService.java
@@ -6,11 +6,9 @@ import java.util.Map;
 import com.hifive.bururung.domain.carshare.participant.dto.AvailableCarShareListResponse;
 import com.hifive.bururung.domain.carshare.participant.dto.CarInformationResponse;
 import com.hifive.bururung.domain.carshare.participant.dto.DriverInformationResponse;
-import com.hifive.bururung.domain.carshare.participant.dto.DrivingInformation;
 
 public interface IServiceRegistrationService {
 	List<AvailableCarShareListResponse> getAvailableCarShareList();
 	DriverInformationResponse getDriverInformation(Long memberId);
 	CarInformationResponse getCarInformation(Long memberId);
-	DrivingInformation getDrivingInformation(Map<String, Object> params);
 }
