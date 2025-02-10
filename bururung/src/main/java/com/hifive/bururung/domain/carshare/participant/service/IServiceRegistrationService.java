e354162da3be1c6877d32b4a7b346d20c6d73ab0
nayeon990315 <99skdus@naver.com>
2025-02-10 09:59:45 +0900

    Feat: 차량 공유 상세 정보 조회

--- bururung/src/main/java/com/hifive/bururung/domain/carshare/participant/service/IServiceRegistrationService.java
+++ bururung/src/main/java/com/hifive/bururung/domain/carshare/participant/service/IServiceRegistrationService.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 import com.hifive.bururung.domain.carshare.participant.dto.AvailableCarShareListResponse;
 import com.hifive.bururung.domain.carshare.participant.dto.CarInformationResponse;
+import com.hifive.bururung.domain.carshare.participant.dto.CarShareRegistrationRequest;
 import com.hifive.bururung.domain.carshare.participant.dto.DriverInformationResponse;
+import com.hifive.bururung.domain.carshare.participant.dto.DrivingInformationResponse;
 
 public interface IServiceRegistrationService {
 	List<AvailableCarShareListResponse> getAvailableCarShareList();
 	DriverInformationResponse getDriverInformation(Long memberId);
 	CarInformationResponse getCarInformation(Long memberId);
+	DrivingInformationResponse getDrivingInformation(Long memberId, Long carShareRegiId);
+	void insertRegistration(CarShareRegistrationRequest request);
 }
