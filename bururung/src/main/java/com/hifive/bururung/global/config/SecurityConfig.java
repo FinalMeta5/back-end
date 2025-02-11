7df87e2036aaeed45c801d6d2d549a0217cdba48
nayeon990315 <99skdus@naver.com>
2025-02-11 09:23:11 +0900

    Feat: 차량 상세 조회 및 예약 기능 구현

--- bururung/src/main/java/com/hifive/bururung/global/config/SecurityConfig.java
+++ bururung/src/main/java/com/hifive/bururung/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hifive.bururung.global.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.hifive.bururung.global.common.JwtAuthenticationFilter;
 import com.hifive.bururung.global.common.TokenProvider;
