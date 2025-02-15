a346964e378b0f5f849853dcb1e8a5c44e19ccb5
gusals <gusals6586@gmail.com>
2025-02-16 02:54:41 +0900

    Fix: ErrorPageFilter 오류 해결 시도

--- bururung/src/main/java/com/hifive/bururung/global/exception/ControllerAdvice.java
+++ bururung/src/main/java/com/hifive/bururung/global/exception/ControllerAdvice.java
@@ -3,14 +3,19 @@ package com.hifive.bururung.global.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestControllerAdvice
+@Slf4j
 public class ControllerAdvice {
 	
     @ExceptionHandler(value = CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e, Http) {
+    	if(response)
         return ErrorResponse.of(e);
     }
     
@@ -22,5 +27,10 @@ public class ControllerAdvice {
         				.code("RUNTIME_ERROR")
         				.status(HttpStatus.BAD_REQUEST)
         				.build());
+    }
+    
+    @ExceptionHandler(value = AuthenticationException.class)
+    public void handleAuthenticationException(AuthenticationException e) {
+        log.warn("Authentication exception occurred: {}", e.getMessage());
     }
 }
