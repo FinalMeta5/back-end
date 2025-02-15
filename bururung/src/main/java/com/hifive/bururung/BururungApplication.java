6a1b72f873814863e6f54f5ab2160044006c0766
gusals <gusals6586@gmail.com>
2025-02-16 03:03:21 +0900

    Fix: ErrorPageFilter 오류 해결 시도

--- bururung/src/main/java/com/hifive/bururung/BururungApplication.java
+++ bururung/src/main/java/com/hifive/bururung/BururungApplication.java
@@ -1,14 +1,15 @@
 package com.hifive.bururung;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
 @EnableRetry
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class BururungApplication extends SpringBootServletInitializer {
 	
 	@Override
--- bururung/src/main/java/com/hifive/bururung/global/common/JwtAuthenticationEntryPoint.java
+++ bururung/src/main/java/com/hifive/bururung/global/common/JwtAuthenticationEntryPoint.java
@@ -31,5 +31,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
             }
             """;
         response.getWriter().write(responseBody);
+        response.flushBuffer();
 	}	
 }
