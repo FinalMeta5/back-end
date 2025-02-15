92163960fcb79697e22597c1067ad617655c89c7
gusals00 <87007552+gusals00@users.noreply.github.com>
2025-02-16 01:47:50 +0900

    Fix: ErrorPageFilter 오류 해결 시

--- bururung/src/main/java/com/hifive/bururung/global/config/WebConfig.java
+++ bururung/src/main/java/com/hifive/bururung/global/config/WebConfig.java
@@ -7,15 +7,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig {
-
     @Bean
-    public ErrorPageFilter customErrorPageFilter() {
-        return new ErrorPageFilter();
-    }
-
-    @Bean
-    public FilterRegistrationBean<ErrorPageFilter> disableErrorPageFilter(ErrorPageFilter customErrorPageFilter) {
-        FilterRegistrationBean<ErrorPageFilter> bean = new FilterRegistrationBean<>(customErrorPageFilter);
+    public FilterRegistrationBean<ErrorPageFilter> disableDefaultErrorPageFilter(ErrorPageFilter filter) {
+        FilterRegistrationBean<ErrorPageFilter> bean = new FilterRegistrationBean<>(filter);
         bean.setEnabled(false);
         return bean;
     }
